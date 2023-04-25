f40572558cce8418fe381b91951a4268829a3725
Johnnatan Barros <johnnatanwillian@hotmail.com>
2023-04-24 22:48:46 -0400

    Included api routes to recipes and ingredients

---
+++ django-recipe-manager/recipes/urls.py
@@ -0,0 +1,73 @@
+from django.urls import path
+from recipes import views
+
+
+urlpatterns = [
+    # Ingredient Views
+    path(
+        "ingredients/",
+        views.IngredientListCreateAPIView.as_view(),
+        name="ingredient_list_create",
+    ),
+    path(
+        "ingredients/<int:pk>/",
+        views.IngredientUpdateAPIView.as_view(),
+        name="ingredient_update",
+    ),
+    path(
+        "ingredients/delete/<int:pk>/",
+        views.IngredientDestroyAPIView.as_view(),
+        name="ingredient_delete",
+    ),
+
+    # Ingredient Price Views
+    path(
+        "ingredients/<int:ingredient_pk>/prices/",
+        views.IngredientPriceListCreateAPIView.as_view(),
+        name="ingredient_price_list_create",
+    ),
+    path(
+        "ingredients/<int:ingredient_pk>/prices/<int:pk>/",
+        views.IngredientPriceUpdateAPIView.as_view(),
+        name="ingredient_price_update",
+    ),
+    path(
+        "ingredients/<int:ingredient_pk>/prices/delete/<int:pk>/",
+        views.IngredientPriceDestroyAPIView.as_view(),
+        name="ingredient_price_delete",
+    ),
+
+    # Recipes views
+    path(
+        "recipes/",
+        views.RecipeListCreateAPIView.as_view(),
+        name="recipe_list_create",
+    ),
+    path(
+        "recipes/<int:pk>/",
+        views.RecipeUpdateAPIView.as_view(),
+        name="recipe_update",
+    ),
+    path(
+        "recipes/delete/<int:pk>/",
+        views.RecipeDestroyAPIView.as_view(),
+        name="recipe_delete",
+    ),
+
+    # Recipe Ingredient views
+    path(
+        "recipes/<int:recipe_pk>/ingredients/",
+        views.RecipeIngredientListCreateAPIView.as_view(),
+        name="recipe_ingredient_list_create",
+    ),
+    path(
+        "recipes/<int:recipe_pk>/ingredients/<int:pk>/",
+        views.RecipeIngredientUpdateAPIView.as_view(),
+        name="recipe_ingredient_update",
+    ),
+    path(
+        "recipes/<int:recipe_pk>/ingredients/delete/<int:pk>/",
+        views.RecipeIngredientDestroyAPIView.as_view(),
+        name="recipe_ingredient_delete",
+    ),
+]
--- django-recipe-manager/recipes/views.py
+++ django-recipe-manager/recipes/views.py
@@ -1,3 +1,98 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+    RetrieveDestroyAPIView,
+)
 
-# Create your views here.
+from recipes.models import Ingredient, IngredientPrice, Recipe, RecipeIngredient
+from recipes.serializers import (
+    IngredientSerializer,
+    IngredientPriceSerializer,
+    RecipeSerializer,
+    RecipeIngredientSerializer
+)
+
+
+class BaseIngredientView:
+    serializer_class = IngredientSerializer
+
+    def get_queryset(self):
+        # return Ingredient list for current user
+        return Ingredient.objects.filter(user=self.request.user)
+
+
+class IngredientListCreateAPIView(BaseIngredientView, ListCreateAPIView):
+    pass
+
+
+class IngredientUpdateAPIView(BaseIngredientView, RetrieveUpdateAPIView):
+    pass
+
+
+class IngredientDestroyAPIView(BaseIngredientView, RetrieveDestroyAPIView):
+    pass
+
+
+class BaseIngredientPriceView:
+    serializer_class = IngredientPriceSerializer
+
+    def get_queryset(self):
+        # return Price list for ingredient
+        ingredients = IngredientPrice.objects.filter(
+            ingredient__user=self.request.user, ingredient=self.kwargs["ingredient_pk"]
+        )
+        return ingredients
+
+
+class IngredientPriceListCreateAPIView(BaseIngredientPriceView, ListCreateAPIView):
+    pass
+
+
+class IngredientPriceUpdateAPIView(BaseIngredientPriceView, RetrieveUpdateAPIView):
+    pass
+
+
+class IngredientPriceDestroyAPIView(BaseIngredientPriceView, RetrieveDestroyAPIView):
+    pass
+
+
+class BaseRecipeView:
+    serializer_class = RecipeSerializer
+
+    def get_queryset(self):
+        # return Recipe list for current user
+        return Recipe.objects.filter(user=self.request.user)
+
+
+class RecipeListCreateAPIView(BaseRecipeView, ListCreateAPIView):
+    pass
+
+
+class RecipeUpdateAPIView(BaseRecipeView, RetrieveUpdateAPIView):
+    pass
+
+
+class RecipeDestroyAPIView(BaseRecipeView, RetrieveDestroyAPIView):
+    pass
+
+
+class BaseRecipeIngredientView:
+    serializer_class = RecipeIngredientSerializer
+
+    def get_queryset(self):
+        # return RecipeIngredient list for current recipe
+        return RecipeIngredient.objects.filter(
+            recipe__user=self.request.user, recipe=self.kwargs["recipe_pk"]
+        )
+
+
+class RecipeIngredientListCreateAPIView(BaseRecipeIngredientView, ListCreateAPIView):
+    pass
+
+
+class RecipeIngredientUpdateAPIView(BaseRecipeIngredientView, RetrieveUpdateAPIView):
+    pass
+
+
+class RecipeIngredientDestroyAPIView(BaseRecipeIngredientView, RetrieveDestroyAPIView):
+    pass
