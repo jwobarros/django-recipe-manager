067e9c1bb80a9ec17704f33334c2dfd806901f5f
Johnnatan Barros <johnnatanwillian@hotmail.com>
2023-04-25 14:24:45 -0400

    Added test to user model

--- django-recipe-manager/recipes/tests.py
+++ django-recipe-manager/recipes/tests.py
@@ -1,3 +1,32 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class UsersManagersTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(username="normaluser", password="foo", email="normal@user.com")
+        self.assertEqual(user.email, "normal@user.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(ValueError):
+            User.objects.create_user(username="")
+        with self.assertRaises(ValueError):
+            User.objects.create_user(username="", password="foo")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="superuser", password="foo", email="super@user.com"
+        )
+        self.assertEqual(admin_user.email, "super@user.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                username="superuser", password="foo", is_superuser=False
+            )
